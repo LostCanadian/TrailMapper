85bee52905e993382b94208d85061de3d8040823
LostCanadian <38511683+LostCanadian@users.noreply.github.com>
2026-02-13 16:13:39 -0800

    Fix GitHub Pages deployment for static site

--- sw.js
+++ sw.js
@@ -1,5 +1,13 @@
-const CACHE = 'trailmapper-v1';
-const ASSETS = ['/', '/index.html', '/styles.css', '/app.js', '/manifest.webmanifest', '/icon.svg'];
+const CACHE = 'trailmapper-v2';
+const BASE = self.location.pathname.replace(/\/[^/]*$/, '/');
+const ASSETS = [
+  BASE,
+  `${BASE}index.html`,
+  `${BASE}styles.css`,
+  `${BASE}app.js`,
+  `${BASE}manifest.webmanifest`,
+  `${BASE}icon.svg`
+];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
